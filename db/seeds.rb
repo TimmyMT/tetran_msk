d2407b0f9a1e587e048b25e6258f1f1a7cdeb3de
Timmy_MT <mathcore_detected@mail.ru>
2019-11-02 18:48:19 +0500

    changed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Customer.create([
+    {name: 'test1', description: "test_description", phone: "123456"},
+    {name: 'test2', description: "test_description", phone: "123456"},
+    {name: 'test3', description: "test_description", phone: "123456"},
+    {name: 'test4', description: "test_description", phone: "123456", black_list_added: true},
+    {name: 'test5', description: "test_description", phone: "123456", black_list_added: true},
+    {name: 'test6', description: "test_description", phone: "123456", black_list_added: true}
+])
