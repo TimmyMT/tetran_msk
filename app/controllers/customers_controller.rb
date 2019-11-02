a8dc2df0d3fb11ba97bf982a8b11530bf8cdd25a
Timmy_MT <mathcore_detected@mail.ru>
2019-11-02 22:53:36 +0500

    added function 'insert black list customer on phone'

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,5 @@
 class CustomersController < ApplicationController
-  before_action :set_customer, only: [:add_to_black_list, :delete_from_black_list, :edit, :update]
+  before_action :set_customer, only: [:add_to_black_list, :delete_from_black_list, :edit, :update, :destroy]
 
   def index
     @customers = Customer.where(black_list_added: false)
@@ -7,6 +7,19 @@ class CustomersController < ApplicationController
 
   def black_list
     @customers = Customer.where(black_list_added: true)
+  end
+
+  def new
+    @customer = Customer.new
+  end
+
+  def create
+    @customer = Customer.new(customer_params)
+    if @customer.save
+      redirect_to customers_path
+    else
+      render :new
+    end
   end
 
   def edit
@@ -19,14 +32,15 @@ class CustomersController < ApplicationController
   end
 
   def add_black_list_on_phone
-    @customer = Customer.find(params[:phone])
+    @customer = Customer.find_by(phone: params[:phone])
     if @customer.present?
       @customer.add_to_black_list
       if @customer.save
         redirect_to bl_customers_path
       end
     else
-      # create new
+      Customer.create!(phone: params[:phone], name: "Unknown", description: "Empty", black_list_added: true)
+      redirect_to bl_customers_path
     end
   end
 
@@ -42,6 +56,11 @@ class CustomersController < ApplicationController
     if @customer.save
       redirect_to customers_path
     end
+  end
+
+  def destroy
+    @customer.destroy
+    redirect_to customers_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
 
   get "/customers/black_list_customers", to: "customers#black_list", as: 'bl_customers'
+  # patch "/customers/add_black_list_on_phone", to: "customers#add_black_list_on_phone", as: 'add_black_list_on_phone'
 
   resources :customers do
     member do
       patch :add_to_black_list
       patch :delete_from_black_list
+    end
+
+    collection do
+      post :add_black_list_on_phone
     end
   end
   root "customers#index"
